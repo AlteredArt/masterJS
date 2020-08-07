cdf4f5d54401f1c494354af77f0feefd4224d760
Altered Art <cherokeedreemer@gmail.com>
2020-08-07 08:50:46 -0600

    functions

--- Arrays/arraysEX.js
+++ Arrays/arraysEX.js
@@ -1,4 +1,4 @@
-// Array of arrays
+// Array of arrays show length of all arrays, the array item, length of array
 let twoD = [[1,2,3,4,5,6,7],
             [8, 10, 5 ,7, 3, 22, 6, 42],
             [123, 54, 12, 11, 9, 15]];
