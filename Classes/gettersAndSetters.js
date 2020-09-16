b10e436c3ab76fa2a60779f5475679a73956f0e9
Altered Art <cherokeedreemer@gmail.com>
2020-09-16 10:58:53 -0600

    finished cleaning up now add more

--- Classes/gettersAndSetters.js
+++ Classes/gettersAndSetters.js
@@ -18,3 +18,32 @@ class Square {
 const s1 = new Square(5); 
 console.log(s1)
 console.log("square area", s1.area)
+
+
+// use getters to access variables
+function Circle (radius){
+    this.radius = radius;
+    let = defaultLocation = {x:0, y:0};
+    this.getDefaultLocation = function(){
+        return defaultLocation
+    };
+    this.draw = function(){
+        console.log("draw");
+    };
+// define a getter and setter
+    Object.defineProperty(this, 'defaultLocation', {
+         get: function() {
+            return defaultLocation;
+        },
+        set: function(value){
+            if (!value.x || !value.y)
+            throw new Error('Invalid Location');
+            defaultLocation = value;
+        }
+    })
+}
+
+const circle = new Circle(10);
+circle.draw();
+// invalid location error
+circle.defaultLocation = 1
